1e3672b7166bfcbb6c7b3b7b1a9e4e5b3f686b7a
Khaled <khaledkhalil4@outlook.com>
2024-07-29 15:29:00 +0300

    Added Packet Class. Maybe needs some adjustments

--- simulator.cpp
+++ simulator.cpp
@@ -9,12 +9,22 @@
 
 using namespace std;
 
-class packet {
+class Packet {
+public:
+    double arrivalTime;
+    double processingTime;
+    double waitTime;
+    double finishTime;
+    double serverID;
+
+    Packet(double arrivalTime, double processingTime, double waitTime = 0, double finishTime = 0, int serverId = -1): //maybe we don't need serverID
+    arrivalTime(arrivalTime), processingTime(processingTime), waitTime(waitTime),finishTime(finishTime),
+        serverID(serverId){};
 
 };
 
 
-class simulator{
+class Simulator{
 
 };
 
